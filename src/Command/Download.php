10cb2f3ae0436aeeb7bd96cf7b8961ad701de596
Simon Dubois <sdu@ppgm.fr>
2015-09-08 13:44:53 +0200

    Base console routine

---
+++ src/Command/Download.php
@@ -0,0 +1,30 @@
+<?php namespace Simondubois\UnsplashDownloader\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class Download extends Command
+{
+    protected function configure()
+    {
+        $this
+            ->setName('download')
+            ->setDescription('Download unsplash photos')
+            ->addOption(
+               'destination',
+               'd',
+               InputOption::VALUE_REQUIRED,
+               'If defined, download photos into the specified directory',
+               '.'
+            )
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $destination = $input->getOption('destination');
+    }
+}
