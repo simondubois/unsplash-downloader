04a8417480737d26abc3e945ca4ee93c3a07ac0b
Simon Dubois <sdu@ppgm.fr>
2015-09-09 13:51:37 +0200

    Handle option destination

--- src/Command/Download.php
+++ src/Command/Download.php
@@ -1,5 +1,6 @@
 <?php namespace Simondubois\UnsplashDownloader\Command;
 
+use Exception;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
@@ -7,6 +8,7 @@ use Symfony\Component\Console\Output\OutputInterface;
 
 class Download extends Command
 {
+
     protected function configure()
     {
         $this
@@ -17,16 +19,65 @@ class Download extends Command
                 'd',
                 InputOption::VALUE_REQUIRED,
                 'If defined, download photos into the specified directory',
-                '.'
+                ''
             )
         ;
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $destination = getcwd().'/'.$input->getOption('destination');
+        $destination = $this->destination($input->getOption('destination'));
         if ($output->getVerbosity() >= OutputInterface::VERBOSITY_VERBOSE) {
             $output->writeln("Save photos to {$destination}");
         }
     }
+
+
+
+    //
+    // Handle option "destination"
+    //
+
+    private function destination($option)
+    {
+        if (substr($option, 0, 1) === '/') {
+            $destination = $this->absoluteDestination($option);
+        } else {
+            $destination = $this->relativeDestination($option);
+        }
+
+        $destination = $this->validDestination($destination);
+
+        return $destination;
+    }
+
+    private function absoluteDestination($option)
+    {
+        return $option;
+    }
+
+    private function relativeDestination($option)
+    {
+        return getcwd().'/'.$option;
+    }
+
+    private function validDestination($destination)
+    {
+        $validDestination = realpath($destination);
+
+        if ($validDestination === false) {
+            throw new Exception("The requested validDestination path ({$destination}) does not exists.");
+        }
+
+        if (is_dir($validDestination) === false) {
+            throw new Exception("The requested validDestination path ({$validDestination}) is not a directory.");
+        }
+
+        if (is_writable($validDestination) === false) {
+            throw new Exception("The requested validDestination path ({$validDestination}) is not writable.");
+        }
+
+        return $validDestination;
+    }
+
 }
