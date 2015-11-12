99458494c9eb02028118f1dc158727ffdde3f8b0
Simon Dubois <simon@dubandubois.com>
2015-11-12 06:46:57 +0100

    Refractor HistoryTest

--- tests/HistoryTest.php
+++ tests/HistoryTest.php
@@ -25,6 +25,53 @@ class HistoryTest extends PHPUnit_Framework_TestCase
     }
 
     /**
+     * Test Simondubois\UnsplashDownloader\Task::save()
+     */
+    public function testSaveBeforeLoad() {
+        $history = new History();
+        $this->assertFalse($history->save());
+    }
+
+    /**
+     * Test Simondubois\UnsplashDownloader\Task::load()
+     */
+    public function testLoadCalledTwice() {
+        // Instantiate file system
+        $root = vfsStream::setup('test')->url();
+        $existingFile = $root.'/existingFile';
+        $content = ['abc', 'def', '', 'ghi'];
+        file_put_contents($existingFile, implode(PHP_EOL, $content));
+
+        // Assert first load
+        $history = new History();
+        $this->assertTrue($history->load($existingFile));
+
+        // Assert second load
+        try {
+            $history->load($existingFile);
+        } catch (Exception $exception) {
+            $this->assertEquals($content, $history->getContent());
+            return;
+        }
+        $this->fail('An expected exception has not been raised.');
+    }
+
+    /**
+     * Test Simondubois\UnsplashDownloader\Task::load()
+     */
+    public function testLoadMissingFile() {
+        // Instantiate file system
+        $root = vfsStream::setup('test')->url();
+        $missingFile = $root.'/missingFile';
+
+        // Assert missing file
+        $history = new History();
+        $this->assertFalse($history->load($missingFile));
+        $this->assertEmpty($history->getContent());
+        $this->assertFalse($history->save());
+    }
+
+    /**
      * Test Simondubois\UnsplashDownloader\Task::load()
      *     & Simondubois\UnsplashDownloader\Task::loadContent()
      *     & Simondubois\UnsplashDownloader\Task::getContent()
@@ -41,15 +88,8 @@ class HistoryTest extends PHPUnit_Framework_TestCase
         $content = ['abc', 'def', '', 'ghi'];
         file_put_contents($existingFile, implode(PHP_EOL, $content));
 
-        // Assert save before load
-        $history = new History();
-        $this->assertFalse($history->save());
-
-        // Assert missing file
-        $this->assertFalse($history->load($missingFile));
-        $this->assertEmpty($history->getContent());
-
         // Assert successful load
+        $history = new History();
         $this->assertTrue($history->load($existingFile));
         $this->assertEquals($content, $history->getContent());
         $this->assertTrue($history->has('abc'));
@@ -65,14 +105,6 @@ class HistoryTest extends PHPUnit_Framework_TestCase
         $this->assertTrue($history->save());
         $this->assertEquals($content, $history->getContent());
         $this->assertEquals(implode(PHP_EOL, $content), file_get_contents($existingFile));
-
-        // Assert second load
-        try {
-            $history->load($existingFile);
-        } catch (Exception $exception) {
-            return;
-        }
-        $this->fail('An expected exception has not been raised.');
     }
 
 }
