b02f6306af488312a30b1eb2dc40d2cbc2e1d02b
Simon Dubois <simon@dubandubois.com>
2015-11-12 06:55:00 +0100

    Refractor HistoryTest

--- tests/HistoryTest.php
+++ tests/HistoryTest.php
@@ -34,6 +34,7 @@ class HistoryTest extends PHPUnit_Framework_TestCase
 
     /**
      * Test Simondubois\UnsplashDownloader\Task::load()
+     *     & Simondubois\UnsplashDownloader\Task::loadContent()
      */
     public function testLoadCalledTwice() {
         // Instantiate file system
@@ -74,16 +75,11 @@ class HistoryTest extends PHPUnit_Framework_TestCase
     /**
      * Test Simondubois\UnsplashDownloader\Task::load()
      *     & Simondubois\UnsplashDownloader\Task::loadContent()
-     *     & Simondubois\UnsplashDownloader\Task::getContent()
      *     & Simondubois\UnsplashDownloader\Task::has()
-     *     & Simondubois\UnsplashDownloader\Task::put()
-     *     & Simondubois\UnsplashDownloader\Task::save()
-     *     & Simondubois\UnsplashDownloader\Task::saveContent()
      */
-    public function testLoadAndSave() {
+    public function testSuccessfulLoad() {
         // Instantiate file system
         $root = vfsStream::setup('test')->url();
-        $missingFile = $root.'/missingFile';
         $existingFile = $root.'/existingFile';
         $content = ['abc', 'def', '', 'ghi'];
         file_put_contents($existingFile, implode(PHP_EOL, $content));
@@ -93,10 +89,33 @@ class HistoryTest extends PHPUnit_Framework_TestCase
         $this->assertTrue($history->load($existingFile));
         $this->assertEquals($content, $history->getContent());
         $this->assertTrue($history->has('abc'));
+        $this->assertTrue($history->has('def'));
         $this->assertTrue($history->has(''));
+        $this->assertTrue($history->has('ghi'));
+        $this->assertFalse($history->has('jkl'));
+    }
+
+    /**
+     * Test Simondubois\UnsplashDownloader\Task::load()
+     *     & Simondubois\UnsplashDownloader\Task::loadContent()
+     *     & Simondubois\UnsplashDownloader\Task::getContent()
+     *     & Simondubois\UnsplashDownloader\Task::has()
+     *     & Simondubois\UnsplashDownloader\Task::put()
+     *     & Simondubois\UnsplashDownloader\Task::save()
+     *     & Simondubois\UnsplashDownloader\Task::saveContent()
+     */
+    public function testSuccessfulSave() {
+        // Instantiate file system
+        $root = vfsStream::setup('test')->url();
+        $existingFile = $root.'/existingFile';
+        $content = ['abc', 'def', '', 'ghi'];
+        file_put_contents($existingFile, implode(PHP_EOL, $content));
+
+        // Instantiate history
+        $history = new History();
+        $history->load($existingFile);
 
         // Assert put & has
-        $this->assertFalse($history->has('jkl'));
         $history->put('jkl');
         $content[] = 'jkl';
         $this->assertTrue($history->has('jkl'));
